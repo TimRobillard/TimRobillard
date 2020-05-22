72e1113928ce69a1e5310bfc8a4c76c05f4ad306
Albatrooss <robillardtim@gmail.com>
2020-05-22 15:31:18 -0400

    MAJOR UPDATE,removed blackjack, first draft of new look

--- index/js.js
+++ index/js.js
@@ -1,8 +1,42 @@
-function dropdownMenu() {
-            var x = document.getElementById("dropdownClick");
-            if(x.className === "topnav") {
-                x.className += " responsive";        
-            }else {
-                x.className = "topnav";
-            }
-        }
+let emailIcon = document.getElementById('email-icon');
+let firstScroll = true;
+let headerLogo = document.querySelector('#logo-bird');
+let headerLogoLeft = document.querySelector('#logo-left');
+let headerLogoRight = document.querySelector('#logo-right');
+
+emailIcon.addEventListener('click', emailClick);
+
+function emailClick(e) {
+  e.preventDefault();
+  window.open(
+    'mailto:robillardtim@gmail.com?subject=' + escape('**ATTENTION** Sent from timrobillard.com'),
+    '_blank'
+  );
+}
+
+window.onscroll = function () {
+  if (window.scrollY === 0) {
+    headerLogo.className = 'logo-on-load';
+    headerLogoLeft.classList.add('logo-bracket-on-load');
+    headerLogoRight.classList.add('logo-bracket-on-load');
+    firstScroll = true;
+  } else if (firstScroll) {
+    headerLogo.className = 'logo-scroll-out';
+    headerLogoLeft.classList.remove('logo-bracket-on-load');
+    headerLogoRight.classList.remove('logo-bracket-on-load');
+    setTimeout(() => {
+      headerLogo.className = 'header-logo-hidden';
+    }, 500);
+    firstScroll = false;
+  }
+};
+
+window.innerWidth < window.innerHeight
+  ? (headerLogo.style.width = '75%')
+  : (headerLogo.style.height = '75%');
+
+setTimeout(function () {
+  headerLogo.className = 'logo-on-load';
+  headerLogoLeft.classList.add('logo-bracket-on-load');
+  headerLogoRight.classList.add('logo-bracket-on-load');
+}, 0);
